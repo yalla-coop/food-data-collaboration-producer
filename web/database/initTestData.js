cd600e26c62851ba11d4e2810722910c73bea1cf
Hassan ElNajjar <hassan@upet.co>
2023-09-02 02:27:16 +0300

    fix import file

--- web/database/initTestData.js
+++ web/database/initTestData.js
@@ -1,6 +1,6 @@
 import { join } from 'path';
 import { readSqlFile as readSQLFileAndQuery } from './connect.js';
-import deleteAllShopifySessionsData from './shopify_sessions/deleteAllShopifySessionsData';
+import deleteAllShopifySessionsData from './shopify_sessions/deleteAllShopifySessionsData.js';
 
 const initTestData = async () => {
   await readSQLFileAndQuery(join(process.cwd(), '/web/database/build.sql'));
