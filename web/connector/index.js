11778df1ea930c1b0e788269fe84104464dafc6b
Simon Dupree <simon@yallacooperative.com>
2024-02-05 18:30:42 +0100

    update way we load connector

--- web/connector/index.js
+++ web/connector/index.js
@@ -9,17 +9,35 @@ import {
 import facets from './thesaurus/facets.json' assert { type: 'json' };
 import measures from './thesaurus/measures.json' assert { type: 'json' };
 import productTypes from './thesaurus/productTypes.json' assert { type: 'json' };
+import { throwError } from '../utils/index.js';
 
-const connector = new Connector();
+let _connector;
+let connected = false;
 
-await Promise.all([
-  connector.loadFacets(JSON.stringify(facets)),
-  connector.loadMeasures(JSON.stringify(measures)),
-  connector.loadProductTypes(JSON.stringify(productTypes))
-]);
+const loadConnectorWithResources = async () => {
+  try {
+    if (!connected) {
+      const connector = new Connector();
+      const resourcePromisesArray = [
+        connector.loadFacets(JSON.stringify(facets)),
+        connector.loadMeasures(JSON.stringify(measures)),
+        connector.loadProductTypes(JSON.stringify(productTypes))
+      ];
+      await Promise.all(resourcePromisesArray);
+
+      connected = true;
+      _connector = connector;
+      return _connector;
+    }
+
+    return _connector;
+  } catch (error) {
+    throwError('Error loading connector', error);
+  }
+};
 
 export {
-  connector,
+  loadConnectorWithResources,
   CatalogItem,
   SuppliedProduct,
   QuantitativeValue,
--- web/connector/mappedProductTypes.js
+++ web/connector/mappedProductTypes.js
@@ -1,25 +1,29 @@
-import { connector } from './index.js';
+import { loadConnectorWithResources } from './index.js';
 
-const groceryStoreBase = connector.PRODUCT_TYPES['LOCAL_GROCERY-STORE'];
-const savoryGroceries = groceryStoreBase.SAVORY_GROCERIES;
-const flourType = savoryGroceries.FLOUR;
-const cannedGoods = groceryStoreBase.CANNED_GOODS;
-const driedGoods = groceryStoreBase.DRIED_GOODS;
+const loadProductTypes = async () => {
+  const connector = await loadConnectorWithResources();
 
-const productTypes = {
-  Cans: cannedGoods,
-  CerealFlakes: driedGoods?.FLAKE,
-  CerealFlour: flourType,
-  BuckwheatFlour: flourType,
-  Seeds: driedGoods?.SEED,
-  Pulses: driedGoods?.PULSE,
-  CerealGrain: driedGoods?.GRAIN,
-  GFFlour: flourType,
-  Ferments: driedGoods?.FERMENT,
-  'Hemp Flour': flourType,
-  Herbs: driedGoods?.DRIED_HERB,
-  Quinoa: driedGoods?.SEED,
-  RoastedSnacks: savoryGroceries?.SNACK
+  const groceryStoreBase = connector.PRODUCT_TYPES['LOCAL_GROCERY-STORE'];
+  const savoryGroceries = groceryStoreBase.SAVORY_GROCERIES;
+  const flourType = savoryGroceries.FLOUR;
+  const cannedGoods = groceryStoreBase.CANNED_GOODS;
+  const driedGoods = groceryStoreBase.DRIED_GOODS;
+
+  return {
+    Cans: cannedGoods,
+    CerealFlakes: driedGoods?.FLAKE,
+    CerealFlour: flourType,
+    BuckwheatFlour: flourType,
+    Seeds: driedGoods?.SEED,
+    Pulses: driedGoods?.PULSE,
+    CerealGrain: driedGoods?.GRAIN,
+    GFFlour: flourType,
+    Ferments: driedGoods?.FERMENT,
+    'Hemp Flour': flourType,
+    Herbs: driedGoods?.DRIED_HERB,
+    Quinoa: driedGoods?.SEED,
+    RoastedSnacks: savoryGroceries?.SNACK
+  };
 };
 
-export default productTypes;
+export default loadProductTypes;
