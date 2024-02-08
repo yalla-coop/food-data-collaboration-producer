2a32454c2776aa80fc13e19c71fda8cabbe3574d
Simon Dupree <simon@yallacooperative.com>
2024-02-08 12:29:09 +0100

    Clean up

--- web/index.js
+++ web/index.js
@@ -4,6 +4,6 @@ const port = process.env.BACKEND_PORT || process.env.PORT || '3000';
 
 const PORT = parseInt(port, 10);
 
-app.listen(8081, () => {
+app.listen(PORT, () => {
   console.log(`🚀 Server ready at http://localhost:${PORT}`);
 });
