3e80af934a1543d27cf693b855e33d89e2edbae0
Simon Dupree <simon@yallacooperative.com>
2024-07-10 12:54:28 +0200

    refactor use case

--- web/api-modules/products/controllers/get-products.js
+++ web/api-modules/products/controllers/get-products.js
@@ -1,91 +1,23 @@
-import shopify from '../../../shopify.js';
-import { getAndAddVariantsToProducts } from '../../../database/variants/variants.js';
-import { getShopifyIdSubstring } from '../../../database/utils/get-shopify-id-substring.js';
-
-async function findProducts(client) {
-  const response = await client.query({
-    data: {
-      query: `{
-        products(first: 250) {
-          edges {
-            node {
-              description
-              handle
-              id
-              images(first: 250) {
-                edges {
-                  node {
-                    src
-                    id
-                    url
-                  }
-                }
-              }
-              productType
-              status
-              title
-              totalVariants
-              tracksInventory
-              totalInventory
-              variants(first: 10) {
-                edges {
-                  node {
-                    compareAtPrice
-                    id
-                    image {
-                      altText
-                      height
-                      id
-                      originalSrc
-                      src
-                      url
-                    }
-                    inventoryPolicy
-                    inventoryQuantity
-                    price
-                    sku
-                    taxable
-                    title
-                  }
-                }
-              }
-              vendor
-            }
-          }
-        }
-      }`
-    }
-  });
-
-  if (response.errors) {
-    console.error('Failed to load Products', JSON.stringify(response.errors));
-    throw new Error('Failed to load Products');
-  }
-
-  return response?.body?.data?.products?.edges.map(({ node: product }) => ({
-    ...product,
-    id: getShopifyIdSubstring(product.id),
-    variants: product?.variants?.edges.map(({ node: variant }) => ({
-      ...variant,
-      id: getShopifyIdSubstring(variant.id)
-    })),
-    images: product?.images?.edges.map((edge) => edge.node)
-  }));
-}
+import ProductUseCases from '../use-cases/index.js';
 
 const getProducts = async (req, res) => {
   try {
     const { shopifySession } = req;
 
-    const client = new shopify.api.clients.Graphql({ session: shopifySession });
-
-    const products = await findProducts(client);
-    const productsWithVariants = await getAndAddVariantsToProducts(products);
-    res.type('application/json');
-    res.send({ products: productsWithVariants });
+    const products = await ProductUseCases.getProducts({
+      session: shopifySession
+    });
+    return res.status(200).json({
+      products,
+      success: true,
+      message: 'Products retrieved successfully'
+    });
   } catch (error) {
-    console.error(error);
-    return res.status(404).send('Unable to find products');
+    console.error('Error retrieving products', error);
+    return res.status(500).json({
+      success: false,
+      message: 'Error retrieving products'
+    });
   }
 };
 
