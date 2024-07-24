0d554571f59ebb1e0662becf5104be6dc2fd43c5
Alex Luker <alex.luker@gmail.com>
2024-07-24 12:19:26 +0100

    record migrations

---
+++ web/database/migrations.sql
@@ -0,0 +1,37 @@
+BEGIN;
+CREATE TABLE IF NOT EXISTS "line_items" (
+    "external_id" SERIAL PRIMARY KEY,
+    "shopify_id" bigint NOT NULL,
+    "variant_id" bigint NOT NULL,
+    "order_id" bigint NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    CONSTRAINT order_id_variant_id UNIQUE (order_id, variant_id)
+);
+CREATE TRIGGER set_timestamp BEFORE
+UPDATE ON "line_items" FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+COMMIT;
+
+BEGIN;
+CREATE TABLE IF NOT EXISTS "orders" (
+    "draft_order_id" bigint PRIMARY KEY,
+    "completed_order_id" bigint NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP NOT NULL DEFAULT NOW()
+);
+CREATE TRIGGER set_timestamp BEFORE
+UPDATE ON "orders" FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+COMMIT;
+
+BEGIN;
+CREATE TABLE IF NOT EXISTS "sales_sessions" (
+    "id" SERIAL PRIMARY KEY,
+    "shopify_order_id" bigint UNIQUE NOT NULL,
+    "reservation_date" TEXT NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP NOT NULL DEFAULT NOW()
+);
+CREATE TRIGGER set_timestamp BEFORE
+UPDATE ON "sales_sessions" FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+COMMIT;
+
