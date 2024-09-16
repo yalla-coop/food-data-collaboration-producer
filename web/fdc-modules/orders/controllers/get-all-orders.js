3701c6ed7900c6a0ddb9712dcb8008f40da22af8
Alex Luker <alex.luker@gmail.com>
2024-09-16 17:06:51 +0100

    only allow the order creator to interact with it via the api

--- web/fdc-modules/orders/controllers/get-all-orders.js
+++ web/fdc-modules/orders/controllers/get-all-orders.js
@@ -1,5 +1,6 @@
 import { getAllLineItems } from '../../../database/line_items/lineItems.js';
 import shopify from '../../../shopify.js';
+import { findCustomer } from './shopify/customer.js';
 import getSession from '../../../utils/getShopifySession.js';
 import { createBulkDfcOrderFromShopify } from '../dfc/dfc-order.js';
 import { findOrders } from './shopify/orders.js';
@@ -9,6 +10,8 @@ const getAllOrders = async (req, res) => {
         const session = await getSession(`${req.params.EnterpriseName}.myshopify.com`)
         const client = new shopify.api.clients.Graphql({ session });
 
+        const customerId = await findCustomer(client, req.user.id);
+
         const {before, after, first, last} = req.query;
 
         if ((before && after) || (before && first) || (after && last) && (before && !last) && (after && !first)){
@@ -17,7 +20,7 @@ const getAllOrders = async (req, res) => {
 
         const draftOrdersWithLineItemMappings = await getAllLineItems();
 
-        const {orders, pageInfo} = await findOrders(client, {before, after, first: Number(first), last: Number(last)});
+        const {orders, pageInfo} = await findOrders(client, customerId, {before, after, first: Number(first), last: Number(last)});
 
         const allDfcOrders = await createBulkDfcOrderFromShopify(orders, draftOrdersWithLineItemMappings, req.params.EnterpriseName);
 
